ae72fd6f6890f48247b09be8948bd22859ae9d3e
Nick Ager <nick.ager@gmail.com>
2019-04-02 11:11:20 +0100

    simplified project
    simplified project; removed Haskell for Mac. Compiles, but takes too long when running.

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ We have three raised beds. My wife challenged me to help her work out which plan
 2) Some plants don't like being close to others and should be planted in different beds
 3) Some plants are susceptible to aphid attack, others repel aphids. Susceptible plants should paired in the same bed with repelling plants.
 
+## Notes
+Created with:
+
+```
+$ stack new GardenPlanner simple --resolver lts-13.11
+```
--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,103 @@
 module Main where
 
-newtype Plant = Plant { extract :: String } deriving Show
+import           Data.List
+import           Control.Monad
+
+data PlantType = Potatoes | Onions | Garlic | Parsley | Cariander | Cucumber | Beetroot | SpringOnions | SweetCorn | Carrots | Leaks | FrenchBeans | Peas | Dill | Radsh deriving (Eq, Show)
+
+data BedProportion = Quarter | Half | Eighth | Tenth deriving (Eq, Show)
+
+data AphidStatus = Susceptible | Repellant | Neutral  deriving (Eq, Show)
+
+data Plant = Plant { name :: PlantType
+                   , size :: BedProportion
+                   , aphidStatus :: AphidStatus
+                   , badNeighbours :: [PlantType]
+                   } deriving (Eq, Show)
+
 
 plants =
-  [ Plant "potatoes"
-  , Plant "onions"
-  , Plant "garlic"
-  , Plant "parsley"
-  , Plant "corrinder"
-  , Plant "cucumbers"
-  , Plant "beetroot"
-  , Plant "spring onions"
+  [ Plant Potatoes     Half    Neutral     [Onions]
+  , Plant Onions       Quarter Neutral     [Garlic]
+  , Plant Garlic       Eighth  Repellant   [Onions]
+  , Plant Parsley      Eighth  Neutral     []
+  , Plant Cariander    Quarter Neutral     []
+  , Plant Cucumber     Half    Neutral     []
+  , Plant Beetroot     Quarter Neutral     []
+  , Plant SpringOnions Quarter Neutral     []
+  , Plant SweetCorn    Half    Neutral     []
+  , Plant Carrots      Quarter Susceptible {-Repellant-}
+                                           []
+  , Plant Leaks        Quarter Neutral     []
+  , Plant FrenchBeans  Eighth  Neutral     []
+  , Plant Peas         Eighth  Neutral     []
+  , Plant Dill         Tenth   Neutral     []
+  , Plant Radsh        Tenth   Neutral     []
   ]
 
-main :: IO ()
+valueToBedProportion Quarter = 2.5
+valueToBedProportion Half    = 5
+valueToBedProportion Eighth  = 1.25
+valueToBedProportion Tenth   = 1
+
+
+--
+
+isAphidSafe plants =
+  let anySusceptible = any ((== Susceptible) . aphidStatus) plants
+      anyRepellant   = any ((== Repellant) . aphidStatus) plants
+  in  (anySusceptible && anyRepellant) || not anySusceptible
+
+areGoodNeighbours plants =
+  let allBadNeighbours = nub $ concat $ map badNeighbours plants
+      plantNames       = map name plants
+  in  noMatchingElements plantNames allBadNeighbours
+
+plantsTotal plants = sum $ map (valueToBedProportion . size) plants
+
+isFull :: [Plant] -> Bool
+isFull plants = let existingTotal = plantsTotal plants in existingTotal > 8 && existingTotal < 14
+
+noMatchingElements :: Eq a => [a] -> [a] -> Bool
+noMatchingElements xs ys = unionLength == (length xs) + (length ys) where unionLength = length $ union xs xs
+--
+
+plantCombinations :: [Plant] -> [[Plant]]
+plantCombinations plants =
+  filter (\c -> length c > 1 && isFull c && areGoodNeighbours c && isAphidSafe c) $ subsequences plants
+
+--
+
+bedCombinations = [ (x, y, z) | let pc = plantCombinations plants, x <- pc, y <- pc, z <- pc, allUnique x y z ]
+
+allUnique :: Eq a => [a] -> [a] -> [a] -> Bool
+allUnique bedA bedB bedC = ((length bedA) + (length bedB) + (length bedC)) == unionLength
+  where unionLength = length $ union bedC $ union bedA bedB
+
+--
+
+displayCombinations cs = do
+  let indexedCs = zip cs [1 ..]
+  mapM_ displayCombinationIndex indexedCs
+
+displayCombinationIndex (c, i) = do
+  putStrLn $ "Combination " ++ show i
+  displayBedCombination c
+
+displayBedCombination (a, b, c) = do
+  displayBed "bed a: " a
+  displayBed "bed b: " b
+  displayBed "bed c: " c
+
+displayBed title plants =
+  let descriptions = map (show . name) plants
+      description  = (concat $ intersperse ", " descriptions) ++ " (" ++ (show $ plantsTotal plants) ++ ")"
+  in  putStrLn $ title ++ description
+
+--
+
 main = do
-  putStrLn "hello world"
+  let c = bedCombinations
+  putStrLn $ "number of bed combinations = " ++ (show $ length c)
+  -- displayCombinations bedCombinations
+
