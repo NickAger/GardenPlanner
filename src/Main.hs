36069d555c693e0f7f274211330fbd7d0911734a
Nick Ager <nick.ager@gmail.com>
2019-04-04 11:29:20 +0100

    Now generates results in a few seconds
    * Much improved `bedCombinations` algorithm thanks to Stephen Wakely
    * Fixed `areGoodNeighbours` due to bug in `noMatchingElements`
    * Experimented with upperbound of `isFull` to  we generate combinations which includes all beds.
    * Display additional output, such as:
    ```
    $ stack exec GardenPlanner
    Number of plant combinations in one bed = 2761
    Number of bed combinations = 14287698
    Number of plants to combine = 15
    Number of plants in best beds = 15
    Number of beds with largest number of plants = 21768
    
    Beds with largest combinations of plants (first two):
    Combination 1
    bed a: Potatoes, Coriander, Cucumber (12.5)
    bed b: Onions, Beetroot, SpringOnions, SweetCorn (12.5)
    bed c: Garlic, Parsley, Carrots, Leaks, FrenchBeans, Peas, Dill, Radish (12.0)
    Combination 2
    bed a: Potatoes, Coriander, Cucumber (12.5)
    bed b: Garlic, Parsley, Beetroot, SweetCorn, Carrots (12.5)
    bed c: Onions, SpringOnions, Leaks, FrenchBeans, Peas, Dill, Radish (12.0)
    ```

--- src/Main.hs
+++ src/Main.hs
@@ -4,17 +4,15 @@ import           Data.List
 import           Control.Monad
 
 data PlantType = Potatoes | Onions | Garlic | Parsley | Coriander | Cucumber | Beetroot | SpringOnions | SweetCorn | Carrots | Leaks | FrenchBeans | Peas | Dill | Radish deriving (Eq, Show)
-
 data BedProportion = Quarter | Half | Eighth | Tenth deriving (Eq, Show)
-
 data AphidStatus = Susceptible | Repellant | Neutral  deriving (Eq, Show)
-
 data Plant = Plant { name :: PlantType
                    , size :: BedProportion
                    , aphidStatus :: AphidStatus
                    , badNeighbours :: [PlantType]
                    } deriving (Eq, Show)
 
+type BedPlants = [Plant]
 
 plants =
   [ Plant Potatoes     Half    Neutral     [Onions]
@@ -54,24 +52,44 @@ areGoodNeighbours plants =
 
 plantsTotal plants = sum $ map (valueToBedProportion . size) plants
 
-isFull :: [Plant] -> Bool
-isFull plants = let existingTotal = plantsTotal plants in existingTotal > 8 && existingTotal < 15
+isFull :: BedPlants -> Bool
+isFull plants = let existingTotal = plantsTotal plants in existingTotal > 8 && existingTotal < 13
 
 noMatchingElements :: Eq a => [a] -> [a] -> Bool
-noMatchingElements xs ys = unionLength == (length xs) + (length ys) where unionLength = length $ union xs xs
+noMatchingElements xs ys = unionLength == length xs + length ys where unionLength = length $ union xs ys
 --
 
-plantCombinations :: [Plant] -> [[Plant]]
+plantCombinations :: [Plant] -> [BedPlants]
 plantCombinations plants =
   filter (\c -> length c > 1 && isFull c && areGoodNeighbours c && isAphidSafe c) $ subsequences plants
 
 --
 
+{-
+Gets the combinations for the first bed, subtracts from the available plants to find the plants not in the first bed, then given
+those plants generates the combinations for the second bed. So a single first bed combination generates multiple second bed 
+combinations. Repeats for the third bed.
+Algorithmn thanks to Stephen.
+Much more efficient than brute-forcing with a uniqueness check, eg:
+```
 bedCombinations = [ (x, y, z) | let pc = plantCombinations plants, x <- pc, y <- pc, z <- pc, allUnique x y z ]
+```
+-}
+bedCombinations :: [(BedPlants, BedPlants, BedPlants)]
+bedCombinations =
+  let x :: [BedPlants]
+      x = plantCombinations plants
 
-allUnique :: Eq a => [a] -> [a] -> [a] -> Bool
-allUnique bedA bedB bedC = ((length bedA) + (length bedB) + (length bedC)) == unionLength
-  where unionLength = length $ union bedC $ union bedA bedB
+      y :: [(BedPlants, BedPlants)]
+      y = concat $ combine <$> x
+  in  concat $ combine2 <$> y
+
+ where
+  combine :: BedPlants -> [(BedPlants, BedPlants)]
+  combine p = (,) <$> pure p <*> plantCombinations (plants \\ p)
+
+  combine2 :: (BedPlants, BedPlants) -> [(BedPlants, BedPlants, BedPlants)]
+  combine2 (p, q) = (\c -> (p, q, c)) <$> plantCombinations (plants \\ union p q)
 
 --
 
@@ -95,12 +113,53 @@ displayBed title plants =
 
 --
 
+containsPlant :: PlantType -> (BedPlants, BedPlants, BedPlants) -> Bool
+containsPlant plantType (bedA, bedB, bedC) =
+  let bedContainsSpecificPlant = bedContainsPlant plantType
+  in  bedContainsSpecificPlant bedA || bedContainsSpecificPlant bedB || bedContainsSpecificPlant bedC
+ where
+  bedContainsPlant :: PlantType -> BedPlants -> Bool
+  bedContainsPlant plantType = any ((== plantType) . name)
+
+--
+
+-- defined as the combinations with the largest number of plants
+bestCombinations :: [(BedPlants, BedPlants, BedPlants)] -> [(BedPlants, BedPlants, BedPlants)]
+bestCombinations combinations =
+  let scoreAdded         = map (\c@(bedA, bedB, bedC) -> (numberOfPlantsInCombination c, bedA, bedB, bedC)) combinations
+      maximumTotalPlants = foldr maximumLength 0 scoreAdded
+      topScores          = filter ((== maximumTotalPlants) . fst4) scoreAdded
+  in  map (\(_, bedA, bedB, bedC) -> (bedA, bedB, bedC)) topScores
+ where
+  fst4 (a, b, c, d) = a
+  maximumLength :: (Int, BedPlants, BedPlants, BedPlants) -> Int -> Int
+  maximumLength scoredCombination maxSoFar =
+    let length1 = fst4 scoredCombination in if length1 >= maxSoFar then length1 else maxSoFar
+
+numberOfPlantsInCombination (bedA, bedB, bedC) = length bedA + length bedB + length bedC
+
+--
+
 main = do
   let pc = plantCombinations plants
-  putStrLn $ "number of plant combinations in one bed = " ++ (show $ length pc)
+  putStrLn $ "Number of plant combinations in one bed = " ++ show (length pc)
 
-  let c                    = bedCombinations
-  let firstTwoCombinations = take 2 c
-  -- putStrLn $ "number of bed combinations = " ++ (show $ length c)
-  displayCombinations firstTwoCombinations
+  let c = bedCombinations
+  putStrLn $ "Number of bed combinations = " ++ show (length c)
+
+  -- let anyPotatoes = any (containsPlant Potatoes) c
+  -- putStrLn $ "anyPotatoes = " ++ show anyPotatoes
+
+  -- let anyCucumber = any (containsPlant Cucumber) c
+  -- putStrLn $ "anyCucumber = " ++ show anyCucumber
+
+  -- let firstTwoCombinations = take 2 c
+  -- displayCombinations firstTwoCombinations
+  let bestBeds = bestCombinations c
+  putStrLn $ "Number of plants to combine = " ++ show (length plants)
+  putStrLn $ "Number of plants in best beds = " ++ show (numberOfPlantsInCombination $ head bestBeds)
+  putStrLn $ "Number of beds with largest number of plants = " ++ show (length bestBeds)
+
+  putStrLn "\nBeds with largest combinations of plants (first two):"
+  displayCombinations $ take 2 bestBeds
 
